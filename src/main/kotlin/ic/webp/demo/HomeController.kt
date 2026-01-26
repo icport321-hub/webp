5d3ad5795fd4e32d7c42a3cc29b61e84af705686
icport321 <icport321@gmail.com>
2026-01-26 22:24:02 +0900

    Initial Spring Boot project

---
+++ src/main/kotlin/ic/webp/demo/HomeController.kt
@@ -0,0 +1,18 @@
+package ic.webp.demo
+
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+
+
+@Controller
+class HomeController{
+
+    @GetMapping("/")
+    fun home(model: Model):String{
+        model.addAttribute("message", "타임리프에서 온 메시지")
+        return "index"
+    }
+}
