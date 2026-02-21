2db9738562392245785f9cb3c288c4845d6d4c37
icport321 <icport321@gmail.com>
2026-02-22 00:20:18 +0900

    adjust web composing

--- src/main/kotlin/ic/webp/demo/controller/HomeController.kt
+++ src/main/kotlin/ic/webp/demo/controller/HomeController.kt
@@ -11,8 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping
 class HomeController{
 
     @GetMapping("/")
-    fun home(model: Model):String{
+    fun home(model: Model, request: HttpServletRequest):String{
         // model.addAttribute("message", "타임리프에서 온 메시지")
+        val userId = request.session.getAttribute("USER_ID")
+        model.addAttribute("isLoggedIn", request.session.getAttribute("USER_ID") != null)
         return "index"
     }
+    
+    @GetMapping("/about")
+    fun about(model: Model, request: HttpServletRequest):String{
+        return "about"
+    }
+    
+    @GetMapping("/projects")
+    fun projects(model: Model, request: HttpServletRequest):String{
+        return "projects"
+    }
+
 }
